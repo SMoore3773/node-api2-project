22b02aa4f1c0b9bc5ddab31885537229b40811fe
SMoore3773 <Smooresculpture@gmail.com>
2020-05-12 20:45:49 -0400

    restart

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const postRouter = require('./postRouter.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts',postRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h2>Post Api</h>
+      `);
+  });
+
+  module.exports = server;
