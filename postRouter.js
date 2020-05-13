f70140c9f69b75368fc45e1ea524f614f62c1fe8
SMoore3773 <Smooresculpture@gmail.com>
2020-05-13 00:08:19 -0400

    slight changes

--- postRouter.js
+++ postRouter.js
@@ -18,12 +18,8 @@ router.post('/', (req,res)=>{
             })
         .catch(err =>{
             res.status(500).json({errorMessage:'There was an error while saving the post to the database'})
-        })
-        
+        }) 
     }
-
-
-
 })
 
 router.get('/', (req,res)=>{
@@ -43,7 +39,7 @@ router.get('/:id',(req,res)=>{
     db.findById(req.params.id)
     .then(post =>{
         if(post.length){
-            res.status(200).json(post[0])
+            res.status(200).json(post)
         }else{res.status(404).json({message:'The post with the specified ID does not exist.'})}
     })
     .catch(err=>{res.status(500).json({error:'The post information could not be retrieved.'})})
@@ -90,7 +86,7 @@ router.get('/:id/comments',(req,res)=>{
         if(comment.length >0 ){
             res.status(200).json(comment)
         }else{
-            res.status(404).json({message: '"The post with the specified ID does not exist."'})
+            res.status(404).json({message: '"The post with this specified ID does not have comments."'})
         }
     })
 })
